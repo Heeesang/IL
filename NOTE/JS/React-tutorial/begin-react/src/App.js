b69980727b3ded2bfa8c070ecf4ab1f566517473
Heesang <ghs050611@naver.com>
2021-10-05 23:32:11 +0900

    JSX.react

--- NOTE/JS/React-tutorial/begin-react/src/App.js
+++ NOTE/JS/React-tutorial/begin-react/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import Hello from './Hello';
 import './App.css';
 
 function App() {
+  const name = 'react'
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //태그는 꼭 닫혀야 한다.
+    //두개 이상의 태그는 감싸야 한다.
+    //js값을 보여줄때 {}감싸기
+    <>
+      <Hello/>
+      <div style = {style}>{name}</div>
+      <div className = 'gray-box'></div>
+    </>  
   );
 }
 
