19e4f059368a116b731bcd7b4830ce5b934dc483
Heesang <ghs050611@naver.com>
2021-10-06 23:44:40 +0900

    InputSample.react

--- NOTE/JS/React-tutorial/begin-react/src/App.js
+++ NOTE/JS/React-tutorial/begin-react/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Counter from './Counter';
-
+import InputSample from './InputSample';
 
 function App() {
   return (
     //태그는 꼭 닫혀야 한다.
     //두개 이상의 태그는 감싸야 한다.
     //js값을 보여줄때 {}감싸기
-    <Counter/>
+      <InputSample/>
   );
 }
 
