3ac3c786db648a3a99ed8f32edda67395d681948
Heesang <ghs050611@naver.com>
2021-10-06 23:28:19 +0900

    useState.react

--- NOTE/JS/React-tutorial/begin-react/src/App.js
+++ NOTE/JS/React-tutorial/begin-react/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
 import Counter from './Counter';
 
 
@@ -9,10 +7,7 @@ function App() {
     //태그는 꼭 닫혀야 한다.
     //두개 이상의 태그는 감싸야 한다.
     //js값을 보여줄때 {}감싸기
-    <Wrapper>
-      <Hello name = "react" color = "red" isSpecial = {true}/>
-      <Hello color = "pink"/>
-    </Wrapper>
+    <Counter/>
   );
 }
 
