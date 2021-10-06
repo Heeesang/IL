6209e6de16bfb03265ba35ed96e5ff83a25e39f0
Heesang <ghs050611@naver.com>
2021-10-06 23:06:29 +0900

    Conditional Rendering.js

--- NOTE/JS/React-tutorial/begin-react/src/App.js
+++ NOTE/JS/React-tutorial/begin-react/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import Hello from './Hello';
-import './App.css';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+
 
 function App() {
-  const name = 'react'
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
   return (
     //태그는 꼭 닫혀야 한다.
     //두개 이상의 태그는 감싸야 한다.
     //js값을 보여줄때 {}감싸기
-    <>
-      <Hello/>
-      <div style = {style}>{name}</div>
-      <div className = 'gray-box'></div>
-    </>  
+    <Wrapper>
+      <Hello name = "react" color = "red" isSpecial = {true}/>
+      <Hello color = "pink"/>
+    </Wrapper>
   );
 }
 
